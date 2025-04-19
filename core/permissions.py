01a6a409cd3c90a316d0ea5b44166d80d12c3cf8
Shakuurally <87001511+shakuurally@users.noreply.github.com>
2025-04-19 03:20:15 +0300

    Enforce authentication for all endpoints except auth

--- core/permissions.py
+++ core/permissions.py
@@ -1,12 +1,5 @@
 from rest_framework import permissions
 
-class AllowAnyForAuth(permissions.BasePermission):
-    """
-    Custom permission to allow any access to auth endpoints
-    """
-    def has_permission(self, request, view):
-        return True
-
 class IsProgrammaticRequest(permissions.BasePermission):
     """
     Custom permission to only allow programmatic requests (no browser access)

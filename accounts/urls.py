d33d8c6ff14f0b83bfeba87e950379b1b5e7aede
abdishakuuralimohamed <abdishakuuralimohamed@gmail.com>
2025-04-30 13:48:14 +0300

    Add refresh token endpoint URL configuration

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('signin/', views.custom_login, name='custom_login'),
     path('signup/', views.register_user, name='register_user'),
     path('profile/', views.user_profile, name='user_profile'),
+    path('refresh/', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]

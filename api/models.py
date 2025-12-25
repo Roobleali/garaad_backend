b2a17415274031a1cbb64c07a62409d0d401f940
rooble <abdishakuuralimohamed@gmail.com>
2025-12-25 22:06:32 +0000

    Fix: Add missing translation import in api/models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import timedelta
 

2026afec913e6d3b77f879acd3a5341b28bda895
rooble <abdishakuuralimohamed@gmail.com>
2025-12-25 22:59:45 +0000

    Fix: Add missing timezone import in api/views.py

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
+from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
